6b576c75239cda2b42178f55a805885d362abba3
vovanty0405 <voty365@gmail.com>
2025-09-15 11:30:51 +0700

    Them Chuong 4

---
+++ Chuong1/HelloWorld.py
@@ -0,0 +1,31 @@
+# Bai8
+for i in range(4):
+    for j in range(9):
+        print('*',end='')
+    print()
+#Bai9
+for i in range(4):
+    for j in range(4):
+        if i==0 or i==3 or j ==0 or j==3:
+            print('*',end='')
+        else:
+            print(' ',end='')
+    print()
+    #Bai10
+chieu_dai_tam_giac_tren = 1
+for i in range(3):
+    print(' ' * (4 - i) + '*', end='')
+    
+    if i == 1:
+        print(' * * *', end='')
+    elif i == 2:
+        print(' * * * * * *', end='')
+    print()
+print(' ' * 4 + '* * *')
+print(' ' * 2 + '* * * * *')
+print('* * * * * * * * * * *')
+for i in range(2):
+    print(' ' * 8 + '* *', end='')
+    print()
+
+
---
+++ Chuong1/Untitled-1.py
@@ -0,0 +1,5 @@
+a = int(input("Nhap so nguyen thu nhat: "))
+b = int(input("Nhap so nguyen thu hai: "))
+tong = a + b
+print("Tong hai so la:", tong)
+  
